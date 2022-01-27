632aaafb2553a7f352119585f710e87b413b479b
MrZeal025 <ryannograles.025.gtis@gmail.com>
2022-01-27 17:55:43 +0800

    added temporary login protector route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation, Navigate, useNavigate } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import './App.css';
 // Login Page
@@ -10,8 +10,34 @@ import ContactTracingLogs from './pages/ContactTracingLogs';
 import AdminManagement from './pages/AdminManagement';
 
 //utilities
+import jwt_decode from 'jwt-decode';
 import ReportGenerator from './pages/ContactTracingLogs/utilities/ReportGenerator';
 import AddLocation from './pages/Locations/utilities/AddLocation';
+
+// axios interceptors
+import axios from 'axios';
+import { setupInterceptorsTo } from "./axios/interceptors";
+import NotFoundRoute from './routes/NotFoundRoute';
+
+setupInterceptorsTo(axios);
+
+function RequireAuth({ children }) {
+  // decode token from the api
+  const token = localStorage.getItem('token');
+  const decodedToken = token ? jwt_decode(token) : null;
+  
+  let location = useLocation();
+
+  if (decodedToken || null) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+}
 
 function App() {
   return (
@@ -28,14 +54,57 @@ function App() {
           <Route path="/login" element={<Login />} />
           
           {/* Main Admin Pages */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/locations" element={<Locations />} />
-          <Route path="/contact-tracing-logs" element={<ContactTracingLogs />} />
-          <Route path="/admin-management" element={<AdminManagement />} />
-
+          <Route 
+            path="/dashboard" 
+            element={
+              <RequireAuth>
+                <Dashboard/>
+              </RequireAuth>
+            } 
+          />
+          <Route 
+            path="/locations" 
+            element={
+              <RequireAuth>
+                <Locations/>
+              </RequireAuth>
+            } 
+          />
+          <Route 
+            path="/contact-tracing-logs" 
+            element={
+              <RequireAuth>
+                <ContactTracingLogs/>
+              </RequireAuth>
+            } 
+          />
+          <Route 
+            path="/admin-management" 
+            element={
+              <RequireAuth>
+                <AdminManagement/>
+              </RequireAuth>
+            } 
+          />
           {/* utilities */}
-          <Route path="/contact-tracing-logs/generate-report" element={<ReportGenerator />} />
-          <Route path="/locations/add-location" element={<AddLocation />} />
+          <Route 
+            path="/contact-tracing-logs/generate-report" 
+            element={
+              <RequireAuth>
+                <ReportGenerator/>
+              </RequireAuth>
+            } 
+          />
+          <Route 
+            path="/locations/add-location" 
+            element={
+              <RequireAuth>
+                <AddLocation/>
+              </RequireAuth>
+            } 
+          />
+          {/* user manual url not catcher */}
+          <Route path="*" element={<NotFoundRoute/>} />
         </Routes>
       </Router>
     </div>
